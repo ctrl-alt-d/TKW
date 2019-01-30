27e5487c0537dc6aa59a2e18d7986daa9e2afe9d
dani herrera <ctrl.alt.d@gmail.com>
2019-01-30 16:38:30 +0100

    WIP. Added special movements

--- src/questions.js
+++ src/questions.js
@@ -1,9 +1,27 @@
 const Q = [
-    { "tecnica":"Olgul makki", "video":"Ns0L-K-8EpU" },
-    { "tecnica":"Montong makki", "video":"zWenAK-MXos"},
-    { "tecnica":"PALKUT MONTONG PIOCHO CHIGUI", "video":"gwaUWWeC1AM"},
-    { "tecnica":"chebipum mok chigui", "video":"YQvbWkYt5II"},
+
+    // maki defensas
+    { "tecnica":"Olgul makki", "video":"Ns0L-K-8EpU", "alt": "" },
+    { "tecnica":"Montong makki", "video":"zWenAK-MXos", "alt": ""},
+    { "tecnica":"PALKUT MONTONG PIOCHO CHIGUI", "video":"gwaUWWeC1AM", "alt": ""},
     
+    // movimientos pum (especiales)
+    { "tecnica":"Kibon chumbi seogi", "video": "is_z4kJU6nA", "alt": "Kibon chumbi seogi"},
+    { "tecnica":"chebipum mok chigui", "video":"YQvbWkYt5II", "alt": ""},
+    { "tecnica": "montong piochok palkup chigi", "video": "gwaUWWeC1AM", "alt": "PALKUT MONTONG PIOCHOk CHIGUI"},
+    { "tecnica": "bituro jansonnal olgul backat maki", "video": "WejuWi4ALHs", "alt": ""},
+    //{ "tecnica": "bochumok chumbi", "video": "", "alt": ""},
+    { "tecnica": "gawi maki", "video": "OGMiZj_VDRw", "alt": ""},
+    //{ "tecnica": "dangkiok ollyo murop chigui", "video": "", "alt": ""},
+    { "tecnica": "oe santul maki", "video": "zjz1XceSfl0", "alt": ""},
+    { "tecnica": "dangkiok tok jirugui", "video": "So60sYvI840", "alt": ""},
+    
+    
+
+
+
+
+
 ];
 
 export default Q;
