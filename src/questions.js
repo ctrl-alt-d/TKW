85a8f80b837f46d43ed6f752a2e9e530cb9d0729
dani herrera <ctrl.alt.d@gmail.com>
2019-01-31 15:19:16 +0100

    bug. makki x maki

--- src/questions.js
+++ src/questions.js
@@ -1,6 +1,6 @@
 const Q = [
 
-    // maki defensas
+    // makki defensas
     { "tecnica":"Olgul makki", "video":"Ns0L-K-8EpU", "alt": "" },
     { "tecnica":"Montong makki", "video":"zWenAK-MXos", "alt": ""},
     { "tecnica":"PALKUT MONTONG PIOCHO CHIGUI", "video":"gwaUWWeC1AM", "alt": ""},
@@ -9,11 +9,11 @@ const Q = [
     { "tecnica":"Kibon chumbi seogi", "video": "is_z4kJU6nA", "alt": "Kibon chumbi seogi"},
     { "tecnica":"chebipum mok chigui", "video":"YQvbWkYt5II", "alt": ""},
     { "tecnica": "montong piochok palkup chigi", "video": "gwaUWWeC1AM", "alt": "PALKUT MONTONG PIOCHOk CHIGUI"},
-    { "tecnica": "bituro jansonnal olgul backat maki", "video": "WejuWi4ALHs", "alt": ""},
+    { "tecnica": "bituro jansonnal olgul backat makki", "video": "WejuWi4ALHs", "alt": ""},
     //{ "tecnica": "bochumok chumbi", "video": "", "alt": ""},
-    { "tecnica": "gawi maki", "video": "OGMiZj_VDRw", "alt": ""},
+    { "tecnica": "gawi makki", "video": "OGMiZj_VDRw", "alt": ""},
     //{ "tecnica": "dangkiok ollyo murop chigui", "video": "", "alt": ""},
-    { "tecnica": "oe santul maki", "video": "zjz1XceSfl0", "alt": ""},
+    { "tecnica": "oe santul makki", "video": "zjz1XceSfl0", "alt": ""},
     { "tecnica": "dangkiok tok jirugui", "video": "So60sYvI840", "alt": ""},
     
     
