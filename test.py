f0e5e97e2b508701158f31e8ca275401b9533e5e
dani herrera <ctrl.alt.d@gmail.com>
2019-04-06 16:40:45 +0200

    fix id buttons

--- test.py
+++ test.py
@@ -10,20 +10,24 @@ driver.get("https://ctrl-alt-d.github.io/TKW/")
 wait = WebDriverWait(driver, 10)
 
 # nivell negre
-element = wait.until( EC.presence_of_element_located((By.ID, "level-5")) )
-driver.find_element_by_id("level-5").click()
+element = wait.until( EC.visibility_of_element_located((By.ID, "level-5")) )
+element.click()
 
 # espero codi video
 element = wait.until( EC.presence_of_element_located((By.ID, "video-codi")) )
 current = next( q for q in Q if q["video"]==element.text )
 
 # clico la resposta
-for r in current["tecnica"].split():
+for r in current["tecnica"].upper().split():
     element = next( e for e in driver.find_elements_by_class_name("opcio") if e.text == r )
     element.click()
-    wait.until( EC.text_to_be_present_in_element( (By.ID, "my-answer"), r ) )
+    _ = wait.until( EC.text_to_be_present_in_element( (By.ID, "my-answer"), r ) )
 
 # click text
 driver.find_element_by_id("test").click()
+
+# click Bravo
+element = wait.until( EC.visibility_of_element_located((By.ID, "ok-button")) )
+element.click()
 
 driver.close()
