4d428ab6ff0e1689dfba106357937c6bb5065193
dani herrera <ctrl.alt.d@gmail.com>
2019-04-06 18:46:26 +0200

    testos

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -1280,7 +1280,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42201" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39943" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
--- test.py
+++ test.py
@@ -4,35 +4,39 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from testQ import Q
+import time    
 
 driver = webdriver.Chrome()
 driver.get("https://ctrl-alt-d.github.io/TKW/")
 wait = WebDriverWait(driver, 10)
-fake_click = lambda: driver.find_element_by_tag_name("fake-div").click()
 # nivell negre
 element = wait.until( EC.visibility_of_element_located((By.ID, "modal-level")) )
 element = wait.until(EC.element_to_be_clickable((By.ID, "level-5")))
-fake_click()
+time.sleep(1)
 element.click()
 
-# espero codi video
-element = wait.until( EC.presence_of_element_located((By.ID, "video-codi")) )
-fake_click()
-current = next( q for q in Q if q["video"]==element.text )
+while True:
+    # espero codi video
+    element = wait.until( EC.presence_of_element_located((By.ID, "video-codi")) )
+    current = next( q for q in Q if q["video"]==element.text )
 
-# clico la resposta
-for r in current["tecnica"].upper().split():
-    element = next( e for e in driver.find_elements_by_class_name("opcio") if e.text == r )
-    element.click()
-    _ = wait.until( EC.text_to_be_present_in_element( (By.ID, "my-answer"), r ) )
+    # clico la resposta
+    for r in current["tecnica"].upper().split():
+        element = next( e for e in driver.find_elements_by_class_name("opcio") if e.text == r )
+        element.click()
+        _ = wait.until( EC.text_to_be_present_in_element( (By.ID, "my-answer"), r ) )
 
-# click text
-fake_click()
-driver.find_element_by_id("test").click()
+    # click text
+    driver.find_element_by_id("test").click()
 
-# click Bravo
-element = wait.until( EC.visibility_of_element_located((By.ID, "ok-button")) )
-fake_click()
-element.click()
+    # click Bravo
+    element = wait.until( EC.visibility_of_element_located((By.ID, "ok-button")) )
+    time.sleep(1)
+    modal = driver.find_element_by_id("modal-ok")
+
+    driver.find_element_by_id("ok-button").click()
+
+    driver.execute_script(( "document.getElementById('ok-button').click()" ) )
+
 
 driver.close()
