102873856a3d5de6e6a5ec089a10735f279457ee
dani herrera <ctrl.alt.d@gmail.com>
2019-04-06 17:29:06 +0200

    treur close

--- test.py
+++ test.py
@@ -8,13 +8,16 @@ from testQ import Q
 driver = webdriver.Chrome()
 driver.get("https://ctrl-alt-d.github.io/TKW/")
 wait = WebDriverWait(driver, 10)
-
+fake_click = lambda: driver.find_element_by_tag_name("body").click()
 # nivell negre
-element = wait.until( EC.visibility_of_element_located((By.ID, "level-5")) )
+element = wait.until( EC.visibility_of_element_located((By.ID, "modal-level")) )
+element = wait.until(EC.element_to_be_clickable((By.ID, "level-5")))
+fake_click()
 element.click()
 
 # espero codi video
 element = wait.until( EC.presence_of_element_located((By.ID, "video-codi")) )
+fake_click()
 current = next( q for q in Q if q["video"]==element.text )
 
 # clico la resposta
@@ -24,10 +27,12 @@ for r in current["tecnica"].upper().split():
     _ = wait.until( EC.text_to_be_present_in_element( (By.ID, "my-answer"), r ) )
 
 # click text
+fake_click()
 driver.find_element_by_id("test").click()
 
 # click Bravo
 element = wait.until( EC.visibility_of_element_located((By.ID, "ok-button")) )
+fake_click()
 element.click()
 
 driver.close()
